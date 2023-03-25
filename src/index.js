1df06e3a4fd956495d4ec099c17344c4756ce318
Vitaly <vitaly@Vitalys-MacBook-Air.local>
2023-03-25 16:02:19 -0600

    set up basic event handling

--- src/index.js
+++ src/index.js
@@ -2,11 +2,19 @@ import { Client } from 'discord.js'
 import { config } from 'dotenv'
 config() //loads any environment var 
 
-const client = new Client({ intents: ['Guilds', 'GuildMessages']}) //allows to recieve events related to guilds and guild messages
+const client = new Client({ intents: ['Guilds', 'GuildMessages', 'MessageContent']}) //allows to recieve events related to guilds and guild messages etc
 const TOKEN = process.env.BOT_TOKEN
 
-console.log(TOKEN)
 client.login(TOKEN)
 
+//ready event is fired when bot is connected
+client.on('ready', () => {
+    console.log(`${client.user.tag} has logged in`) 
+})
 
+//fired when user sends a message
+client.on('messageCreate', (message) => {
+    console.log(message.content)
+    console.log(message.createdAt.toDateString())
+})
 
